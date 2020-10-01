22fbf8011d7bc9f435860af4873a435235dde683
melchorgabriellopez <48580537+melchorgabriellopez@users.noreply.github.com>
2020-10-01 07:29:35 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,32 @@ namespace ejemplos5_1
     {
         static void Main(string[] args)
         {
-        
+
+            //  [ambito] (tipo de dato) Nombre variable [valor];
+
+            // public,private y protected
+
+            //numero, caracateres, boleanos , objetos y de usuario
+            //int  2.147.483.648 a 2.147.483.647, float,double, decimal,long ,short 32 768, byte 255,  
+            //string
+            //bool true o false
+
+            // private int IdEmpleado = 1;
+
+            int IdAlumno;
+
+            string Nombre;
+
+            DateTime FechaNacimiento;
+
+            float Calificacion;
+
+            bool Estatus;
+
+
+
+
+
         }
     }
 }
